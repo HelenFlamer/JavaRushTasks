3cdc16e94262c1bbf9ba00d6cd8ad060876c8b6f
elena.slinkova <elena.slinkova@fxclub.org>
2017-05-17 16:30:54 +0200

    new tasks

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,22 +1,59 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
+import com.sun.org.apache.regexp.internal.RE;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
  * Created by elena.slinkova on 16.05.2017.
- 6. В самом конце метода main в класса Archiver добавь код, который создает объект типа ExitCommand и вызывает у него метод execute()
- 7. Попробуй, как это все работает
- Обрати внимание, что все файлы проекта должны быть в кодировке UTF-8. Кодировку в IntelliJ IDEA можно задать через пункты меню Settings -> Editor -> File Encodings. Проверь, что все три поля отвечающие за кодировку выставлены в UTF-8.
-
-
+ * <p>
+ * 2. Перепиши метод main():
+ * 2.1. Объяви локальную переменную типа Operation
+ * 2.2. В цикле запрашивай новое значение для переменной п.2.1.
+ * с помощью метода askOperation() и вызывай выполнение операции с помощью CommandExecutor.execute()
+ * 2.3. Обеспечь выход из цикла, если пользователь выбрал операцию Operation.EXIT
+ * 2.4. Оберни вызов askOperation() и execute(operation) в блок try-catch. Если произойдет исключение WrongZipFileException выведи сообщение
+ * «Вы не выбрали файл архива или выбрали неверный файл.» с помощью ConsoleHelper,
+ * при любых других исключениях выводи «Произошла ошибка. Проверьте введенные данные.».
+ * 2.5. Проследи, чтобы программа продолжила свою работу (перешла на новый шаг цикла), после обработки исключений.
+ * 3. Запусти программу и проверь, что команда “выход” работает.
  */
 public class Archiver {
-    public static void main(String[] args){
-        System.out.println("Enter the full path to the file (with its name), where to unzip the file");
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                "0 - упаковать файлы в архив\n" +
+                "1 - добавить файл в архив\n" +
+                "2 - удалить файл из архива\n" +
+                "3 - распаковать архив\n" +
+                "4 - просмотреть содержимое архива\n" +
+                "5 – выход");
+
+        int operationSelected = ConsoleHelper.readInt();
+
+        return Operation.values()[operationSelected];
+    }
+
+    public static void main(String[] args) {
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+
+        } while (operation != Operation.EXIT);
+
+
+       /* System.out.println("Enter the full path to the file (with its name), where to unzip the file");
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         ZipFileManager archivePath = new ZipFileManager(Paths.get(scanner.nextLine()));
         System.out.println("Enter the full path to the file (with its name) you want to archive");
@@ -29,6 +66,6 @@ public class Archiver {
             new ExitCommand().execute();
         } catch (Exception e) {
 
-        }
+        }*/
     }
 }
