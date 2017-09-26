c2a4ece74cede7e66cc72a9b6ccdbac7f9f8bc3f
elena.slinkova <elena.slinkova@fxclub.org>
2017-09-26 17:41:42 +0200

    done

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
@@ -6,24 +6,26 @@ package com.javarush.task.task25.task2506;
  */
 public class LoggingStateThread extends Thread {
 
-    private final Thread target;
-
-    @Override
-    public synchronized void run() {
-        State state = target.getState();
-        System.out.println(state.toString());
-
-        while (state != State.TERMINATED) {
-            if (target.getState() != state) {
-                state = target.getState();
-                System.out.println(state.toString());
-            }
-        }
-    }
-
-    public LoggingStateThread(Thread target) {
-
+    Thread target;
+    public LoggingStateThread(Thread target)
+    {
         this.target = target;
         setDaemon(true);
+
+    }
+
+    @Override
+    public void run()
+    {
+        State state = this.target.getState();
+        System.out.println(state);
+        while (state != State.TERMINATED)
+        {
+            if (state != this.target.getState())
+            {
+                state = this.target.getState();
+                System.out.println(state);
+            }
+        }
     }
 }
