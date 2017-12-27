a43129139e950551808a58e14f16cafaa2943a76
elena.slinkova <elena.slinkova@fxclub.org>
2017-12-27 15:37:29 +0100

    done

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Home.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+public class Home extends GameObject {
+    public Home(int x, int y) {
+        super(x, y);
+        this.setHeight(2);
+        this.setWidth(2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.red);
+        graphics.drawOval(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+
+    }
+}
